c9d390e8158677542250cb66e52d5adc0147ed33
Natalia Castro <nat.alvz@gmail.com>
2018-06-12 22:42:29 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,9 +16,8 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     #end
-    def get_courses
-  self.get_page.css(".post")
-end
+    
   end
+ 
  
 end
